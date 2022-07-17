4fa4ed66ece25d52b9d8e6de7d2f9b1bee8fbe77
abhishekpaul5196 <abhishekpaul5196@gmail.com>
2022-07-17 10:37:51 +0530

    Update VerifyRCB.java
    1. Update IPLTeam object name to rcbTeam from iplTeam.
    2. Added verify foreign player count testscript

--- RCBAutomation/src/test/java/VerifyRCB.java
+++ RCBAutomation/src/test/java/VerifyRCB.java
@@ -1,8 +1,6 @@
-import com.mongodb.util.JSON;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -11,30 +9,50 @@ import java.io.*;
 public class VerifyRCB {
 
     JSONObject teamObj;
-    IPLTeam iplTeam;
+    IPLTeam rcbTeam;
+
     @BeforeTest
     public void addRCBTeam() {
         try {
             teamObj = new JSONObject();
-            JSONParser parser=new JSONParser();
-            FileReader reader=new FileReader("src/main/resources/RCBTeam.json");
-            teamObj=(JSONObject)parser.parse(reader);
-            String teamName=teamObj.get("name").toString();
-            String teamLocation=teamObj.get("location").toString();
-            iplTeam=new IPLTeam(teamName,teamLocation);
-            JSONArray players=(JSONArray) teamObj.get("player");
-            for(int i=0;i<players.size();i++)
-            {
-                JSONObject playerObj=(JSONObject)players.get(i);
-                String playerName=(String) playerObj.get("name");
-                String playerCounty=(String) playerObj.get("country");
-                String playerRole=(String) playerObj.get("role");
-                String playerPrice=(String) playerObj.get("price-in-crores");
-                iplTeam.addPlayer(playerName,playerCounty,playerRole,playerPrice);
+            JSONParser parser = new JSONParser();
+            FileReader reader = new FileReader("src/main/resources/RCBTeam.json");
+            teamObj = (JSONObject) parser.parse(reader);
+            String teamName = teamObj.get("name").toString();
+            String teamLocation = teamObj.get("location").toString();
+            rcbTeam = new IPLTeam(teamName, teamLocation);
+            JSONArray players = (JSONArray) teamObj.get("player");
+            for (int i = 0; i < players.size(); i++) {
+                JSONObject playerObj = (JSONObject) players.get(i);
+                String playerName = (String) playerObj.get("name");
+                String playerCounty = (String) playerObj.get("country");
+                String playerRole = (String) playerObj.get("role");
+                String playerPrice = (String) playerObj.get("price-in-crores");
+                rcbTeam.addPlayer(playerName, playerCounty, playerRole, playerPrice);
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
+    @Test
+    public void verifyForeignPlayers() {
+        try {
+                int countForeignPlayers=0;
+                for(int i=0;i<rcbTeam.playerList.size();i++)
+                {
+                    String countryName=rcbTeam.playerList.get(i).playerCountry;
+                    if(!countryName.equals("India"))
+                        countForeignPlayers++;
+                }
+            if(countForeignPlayers<=4)
+                System.out.println("Test Case: Passed");
+            else
+            {
+                System.out.println("Actual count of foreign players:"+countForeignPlayers+"\nTest Case: Failed");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
