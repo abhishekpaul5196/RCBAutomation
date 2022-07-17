b05ab478002a68ef9b3e7d27897413bc4fd68fda
abhishekpaul5196 <abhishekpaul5196@gmail.com>
2022-07-17 08:59:30 +0530

    Add maven dependencies
    Added json-simple for json parsing and testng for annotation and execution

---
+++ RCBAutomation/src/test/java/VerifyRCB.java
@@ -0,0 +1,13 @@
+import org.json.simple.JSONObject;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class VerifyRCB {
+
+    @BeforeTest
+    public void addRCBTeam()
+    {
+        JSONObject
+    }
+
+}
